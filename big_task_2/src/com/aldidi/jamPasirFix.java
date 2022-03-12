60499ad2946a467dbcd69f82dd089c10660f12b4
aldididi <aldyh024@gmail.com>
2022-03-12 13:21:01 +0700

    add sand clock

---
+++ big_task_2/src/com/aldidi/jamPasirFix.java
@@ -0,0 +1,38 @@
+package com.aldidi;
+
+import java.util.Scanner;
+
+public class jamPasirFix {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("berapa baris bintang?");
+        int baris = scanner.nextInt();
+
+        for (int m = 1; m <= baris; m++)
+        {
+            for (int n = 1; n < m; n++)
+            {
+                System.out.print(" ");
+            }
+            for (int p = m; p <= baris; p++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int m = baris-1; m >= 1; m--)
+        {
+            for (int n = 1; n < m; n++)
+            {
+                System.out.print(" ");
+            }
+            for (int p = m; p <= baris; p++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
